4864e0892baf2ac3bb7daf4b7e183b4f334b77f6
leanshi2018 <szleanshi@163.com>
2019-08-06 17:21:27 +0800

    2222

--- trunk/shop/src/main/java/com/framework/loippi/service/impl/user/RetailProfitServiceImpl.java
+++ trunk/shop/src/main/java/com/framework/loippi/service/impl/user/RetailProfitServiceImpl.java
@@ -1,18 +1,16 @@
 package com.framework.loippi.service.impl.user;
 
-import com.framework.loippi.dao.order.ShopOrderLogisticsDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.framework.loippi.dao.user.RetailProfitDao;
 import com.framework.loippi.entity.user.RetailProfit;
 import com.framework.loippi.service.impl.GenericServiceImpl;
 import com.framework.loippi.service.user.RetailProfitService;
-import com.framework.loippi.support.Page;
-import com.framework.loippi.support.Pageable;
-
-import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
-
+@Service
+@Transactional
 public class RetailProfitServiceImpl extends GenericServiceImpl<RetailProfit, Long> implements RetailProfitService {
-    @Resource
+    @Autowired
     private RetailProfitDao retailProfitDao;
 }
