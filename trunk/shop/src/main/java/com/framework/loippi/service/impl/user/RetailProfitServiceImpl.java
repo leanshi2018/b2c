089a11710f20ecf7b8e1d23ec335f9f63f02342b
leanshi2018 <szleanshi@163.com>
2019-08-06 17:20:07 +0800

    零售利润修改 版本1

--- trunk/shop/src/main/java/com/framework/loippi/service/impl/user/RetailProfitServiceImpl.java
+++ trunk/shop/src/main/java/com/framework/loippi/service/impl/user/RetailProfitServiceImpl.java
@@ -7,12 +7,16 @@ import com.framework.loippi.service.impl.GenericServiceImpl;
 import com.framework.loippi.service.user.RetailProfitService;
 import com.framework.loippi.support.Page;
 import com.framework.loippi.support.Pageable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
-
+@Service
+@Transactional
 public class RetailProfitServiceImpl extends GenericServiceImpl<RetailProfit, Long> implements RetailProfitService {
-    @Resource
+    @Autowired
     private RetailProfitDao retailProfitDao;
 }
