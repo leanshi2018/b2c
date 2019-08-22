82737a54ac7b52319cc656572713b7b80dfb8897
kainoa <lkainoa1121@foxmail.com>
2019-08-22 11:00:39 +0800

    添加获取七牛云凭证接口

--- trunk/plug/src/main/java/com/framework/loippi/utils/qiniu/QiniuConfig.java
+++ trunk/plug/src/main/java/com/framework/loippi/utils/qiniu/QiniuConfig.java
@@ -1,6 +1,9 @@
 package com.framework.loippi.utils.qiniu;
 
 import java.util.Map;
+import java.util.UUID;
+
+import com.qiniu.util.Auth;
 
 /**
  * Created by longbh on 2019/1/8.
@@ -14,12 +17,39 @@ public class QiniuConfig {
     public static String bucket = "member";
 
     public static String qiniuLink = "http://rdnmall.com/";
+
+    Auth auth = Auth.create(APP_ID, APP_SECRET);
     
     public static void initPayConfig(Map<String, Object> dto) {
         QiniuConfig.APP_ID = dto.get("appId") + "";
         QiniuConfig.bucket = dto.get("bucket") + "";
         QiniuConfig.APP_SECRET = dto.get("appSecret") + "";
         QiniuConfig.qiniuLink = dto.get("qiniuLink") + "";
+    }
+
+    public static void getFileName() {
+    }
+
+    /**
+     * 获取凭证
+     * @param bucketName 空间名称
+     * @param key 如果需要覆盖上传则设置此参数
+     * @return
+     */
+    public String getUpToken(String bucketName,String key) {
+        return auth.uploadToken(bucketName);
+    }
+
+    /**
+     * 返回uuid命名的文件名称
+     *
+     * @param fileName
+     * @return
+     */
+    public static String getFileName(String fileName) {
+        String prefix = UUID.randomUUID().toString().replaceAll("-", "");
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        return prefix + suffix;
     }
 
 }
