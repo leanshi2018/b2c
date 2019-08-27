5ef9010620a1a9fc59bbcd1ce760f49945f8bc26
leanshi2018 <szleanshi@163.com>
2019-08-27 15:02:51 +0800

    1111

--- trunk/shop/src/main/java/com/framework/loippi/result/app/order/OrderSubmitResult.java
+++ trunk/shop/src/main/java/com/framework/loippi/result/app/order/OrderSubmitResult.java
@@ -1,18 +1,15 @@
 package com.framework.loippi.result.app.order;
 
-import com.framework.loippi.entity.integration.RdMmIntegralRule;
-import com.framework.loippi.entity.order.ShopOrderPay;
-import com.framework.loippi.entity.user.RdMmAccountInfo;
-import com.framework.loippi.utils.NumberUtils;
-import com.framework.loippi.utils.validator.DateUtils;
-
-import java.math.BigDecimal;
-import java.util.Map;
-import java.util.Optional;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import com.framework.loippi.entity.integration.RdMmIntegralRule;
+import com.framework.loippi.entity.order.ShopOrderPay;
+import com.framework.loippi.entity.user.RdMmAccountInfo;
 
 /**
  * 提交订单-返回app数据
@@ -48,7 +45,7 @@ public class OrderSubmitResult {
     public static OrderSubmitResult build(RdMmIntegralRule rdMmIntegralRule,ShopOrderPay orderPay, RdMmAccountInfo rdMmAccountInfo) {
         OrderSubmitResult orderSubmitResult = new OrderSubmitResult();
         orderSubmitResult.setOrderPaySn(orderPay.getPaySn());
-        orderSubmitResult.setIntegration(rdMmAccountInfo.getWalletBlance());
+        orderSubmitResult.setIntegration(rdMmAccountInfo.getWalletBlance().setScale(2));
         orderSubmitResult.setOrderTotalPrice(orderPay.getPayAmount());
         if (rdMmIntegralRule==null || rdMmIntegralRule.getShoppingPointSr()==null){
             orderSubmitResult.setProportion(0d);
