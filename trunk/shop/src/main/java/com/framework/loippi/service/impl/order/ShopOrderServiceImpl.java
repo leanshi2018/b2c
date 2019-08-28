9652c93f7f2801b8d4dadc3a15313ef393bf8cfd
leanshi2018 <szleanshi@163.com>
2019-08-28 11:13:04 +0800

    3333

--- trunk/shop/src/main/java/com/framework/loippi/service/impl/order/ShopOrderServiceImpl.java
+++ trunk/shop/src/main/java/com/framework/loippi/service/impl/order/ShopOrderServiceImpl.java
@@ -629,7 +629,7 @@ public class ShopOrderServiceImpl extends GenericServiceImpl<ShopOrder, Long> im
                 returnPoint(order.getBuyerId(), order,orderLog.getOperator());
             }
         }
-        if(order.getPaymentState()==0&&order.getUsePointNum()>0){
+        if(order.getPaymentState()==0&&null!=order.getUsePointNum()&&order.getUsePointNum()>0){
             //退回积分
             returnPoint(order.getBuyerId(), order,orderLog.getOperator());
         }
