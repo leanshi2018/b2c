b92607355d79269746d9c22b9bd4fd4985f84aea
kainoa <lkainoa1121@foxmail.com>
2019-09-25 16:34:56 +0800

    aa

--- trunk/admin/src/main/java/com/framework/loippi/controller/trade/OrderSysController.java
+++ trunk/admin/src/main/java/com/framework/loippi/controller/trade/OrderSysController.java
@@ -230,48 +230,50 @@ public class OrderSysController extends GenericController {
                             Map<String, Object> resMap = orderShip(id);//发货返回信息
                             String resultS = (String) resMap.get("res");
                             if (!"".equals(resultS)) {
-                                Map maps = (Map) JSON.parse(resultS);
-                                String success = (String) maps.get("success");//是否成功
-                                String orderSn = (String) maps.get("CsRefNo");//订单编号
-                                if (success.equals("success")) {//发货成功
-                                    String trackingNo = (String) maps.get("TrackingNo");//运单号
-                                    if (!Character.isDigit(trackingNo.charAt(0))) {//不是数字，就是发货失败，有可能在草稿箱
+                                if(!resultS.equals("生成订单出错！System.ArgumentOutOfRangeException: 长度不能小于 0。")){
+                                    Map maps = (Map) JSON.parse(resultS);
+                                    String success = (String) maps.get("success");//是否成功
+                                    String orderSn = (String) maps.get("CsRefNo");//订单编号
+                                    if (success.equals("success")) {//发货成功
+                                        String trackingNo = (String) maps.get("TrackingNo");//运单号
+                                        if (!Character.isDigit(trackingNo.charAt(0))) {//不是数字，就是发货失败，有可能在草稿箱
+                                            String failInfo = (String) maps.get("Info");//失败信息
+                                            System.out.println("failInfo");
+                                            orderService.updateOrderStatus(orderSn, 20, 20, failInfo, "");
+                                        } else {
+                                            //TrackingNo第一个字符是数字
+                                            if (!"".equals(trackingNo)) {// 订单状态：待收货 提交状态：已提交 失败原因：""  +运单号
+                                                System.out.println("待收货");
+                                                Integer orderState = 30;
+                                                Integer submitStatus = 10;
+                                                String failInfo = "";
+                                                orderService.updateOrderStatus(orderSn, orderState, submitStatus, failInfo, trackingNo);
+                                            } else {//状订单态：仓库在备货 提交状态：已提交 失败原因：""
+                                                System.out.println("仓库在备货");
+                                                orderService.updateOrderStatus(orderSn, 25, 10, "", "");
+                                            }
+
+                                            List<Map<String, Object>> products = (List<Map<String, Object>>) resMap.get("Products");//发货的数据
+                                            for (Map<String, Object> product : products) {
+                                                String sku = (String) product.get("SKU");//发货商品规格编号
+                                                Integer quantity = Integer.valueOf(product.get("MaterialQuantity").toString());//发货商品数量
+                                                ShopGoodsSpec goodsSpec = shopGoodsSpecService.findByspecGoodsSerial(sku);//商品规格信息
+                                                Long goodsSpecId = goodsSpec.getId();//商品规格id
+                                                inventoryWarningService.updateInventoryByWareCodeAndSpecId("20192514", goodsSpecId, quantity);
+                                            }
+
+                                            List<ShopOrderGoods> shopOrderGoodsList = new ArrayList<>();
+                                            List<ShopOrderGoods> orderGoodsList = (List<ShopOrderGoods>) resMap.get("orderGoods");
+                                            List<ShopOrderGoods> shopOrderGoods = updateOrderGoods(shopOrderGoodsList, orderGoodsList, trackingNo);//需要修改订单商品信息
+                                            shopOrderGoodsService.updateBatchForShipmentNum(shopOrderGoods);//修改订单商品信息
+                                        }
+
+                                    }
+                                    if (success.equals("failure")) {//发货失败   提交状态：提交失败 失败原因：failInfo
                                         String failInfo = (String) maps.get("Info");//失败信息
                                         System.out.println("failInfo");
                                         orderService.updateOrderStatus(orderSn, 20, 20, failInfo, "");
-                                    } else {
-                                        //TrackingNo第一个字符是数字
-                                        if (!"".equals(trackingNo)) {// 订单状态：待收货 提交状态：已提交 失败原因：""  +运单号
-                                            System.out.println("待收货");
-                                            Integer orderState = 30;
-                                            Integer submitStatus = 10;
-                                            String failInfo = "";
-                                            orderService.updateOrderStatus(orderSn, orderState, submitStatus, failInfo, trackingNo);
-                                        } else {//状订单态：仓库在备货 提交状态：已提交 失败原因：""
-                                            System.out.println("仓库在备货");
-                                            orderService.updateOrderStatus(orderSn, 25, 10, "", "");
-                                        }
-
-                                        List<Map<String, Object>> products = (List<Map<String, Object>>) resMap.get("Products");//发货的数据
-                                        for (Map<String, Object> product : products) {
-                                            String sku = (String) product.get("SKU");//发货商品规格编号
-                                            Integer quantity = Integer.valueOf(product.get("MaterialQuantity").toString());//发货商品数量
-                                            ShopGoodsSpec goodsSpec = shopGoodsSpecService.findByspecGoodsSerial(sku);//商品规格信息
-                                            Long goodsSpecId = goodsSpec.getId();//商品规格id
-                                            inventoryWarningService.updateInventoryByWareCodeAndSpecId("20192514", goodsSpecId, quantity);
-                                        }
-
-                                        List<ShopOrderGoods> shopOrderGoodsList = new ArrayList<>();
-                                        List<ShopOrderGoods> orderGoodsList = (List<ShopOrderGoods>) resMap.get("orderGoods");
-                                        List<ShopOrderGoods> shopOrderGoods = updateOrderGoods(shopOrderGoodsList, orderGoodsList, trackingNo);//需要修改订单商品信息
-                                        shopOrderGoodsService.updateBatchForShipmentNum(shopOrderGoods);//修改订单商品信息
                                     }
-
-                                }
-                                if (success.equals("failure")) {//发货失败   提交状态：提交失败 失败原因：failInfo
-                                    String failInfo = (String) maps.get("Info");//失败信息
-                                    System.out.println("failInfo");
-                                    orderService.updateOrderStatus(orderSn, 20, 20, failInfo, "");
                                 }
                             }
                         }
@@ -287,46 +289,48 @@ public class OrderSysController extends GenericController {
                 Map<String, Object> resMap = orderShip(shopOrder.getId());//发货返回信息
                 String resultS = (String)resMap.get("res");
                 if (!"".equals(resultS)){
-                    Map maps = (Map) JSON.parse(resultS);
-                    String success = (String)maps.get("success");//是否成功
-                    String orderSn = (String)maps.get("CsRefNo");//订单编号
-                    if (success.equals("success")){//发货成功
-                        String trackingNo = (String)maps.get("TrackingNo");//运单号
-                        if (!Character.isDigit(trackingNo.charAt(0))) {//不是数字，就是发货失败，有可能在草稿箱
-                            String failInfo = (String)maps.get("Info");//失败信息
-                            System.out.println("failInfo");
-                            orderService.updateOrderStatus(orderSn,20,20,failInfo,"");
-                        }else {
-                            //TrackingNo第一个字符是数字
-                            if (!"".equals(trackingNo)){// 订单状态：待收货 提交状态：已提交 失败原因："" +运单号
-                                System.out.println("待收货");
-                                Integer orderState = 30;
-                                Integer submitStatus = 10;
-                                String failInfo = "";
-                                orderService.updateOrderStatus(orderSn,orderState,submitStatus,failInfo,trackingNo);
-                            }else{//状订单态：仓库在备货 提交状态：已提交 失败原因：""
-                                System.out.println("仓库在备货");
-                                orderService.updateOrderStatus(orderSn,25,10,"","");
-                            }
-                            List<Map<String,Object>> products = (List<Map<String,Object>>)resMap.get("Products");//发货的数据
-                            for (Map<String, Object> product : products) {
-                                String sku = (String)product.get("SKU");//发货商品规格编号
-                                Integer quantity = Integer.valueOf(product.get("MaterialQuantity").toString());//发货商品数量
-                                ShopGoodsSpec goodsSpec = shopGoodsSpecService.findByspecGoodsSerial(sku);//商品规格信息
-                                Long goodsSpecId = goodsSpec.getId();//商品规格id
-                                inventoryWarningService.updateInventoryByWareCodeAndSpecId("20192514",goodsSpecId,quantity);
-                            }
+                    if(!resultS.equals("生成订单出错！System.ArgumentOutOfRangeException: 长度不能小于 0。")) {
+                        Map maps = (Map) JSON.parse(resultS);
+                        String success = (String) maps.get("success");//是否成功
+                        String orderSn = (String) maps.get("CsRefNo");//订单编号
+                        if (success.equals("success")) {//发货成功
+                            String trackingNo = (String) maps.get("TrackingNo");//运单号
+                            if (!Character.isDigit(trackingNo.charAt(0))) {//不是数字，就是发货失败，有可能在草稿箱
+                                String failInfo = (String) maps.get("Info");//失败信息
+                                System.out.println("failInfo");
+                                orderService.updateOrderStatus(orderSn, 20, 20, failInfo, "");
+                            } else {
+                                //TrackingNo第一个字符是数字
+                                if (!"".equals(trackingNo)) {// 订单状态：待收货 提交状态：已提交 失败原因："" +运单号
+                                    System.out.println("待收货");
+                                    Integer orderState = 30;
+                                    Integer submitStatus = 10;
+                                    String failInfo = "";
+                                    orderService.updateOrderStatus(orderSn, orderState, submitStatus, failInfo, trackingNo);
+                                } else {//状订单态：仓库在备货 提交状态：已提交 失败原因：""
+                                    System.out.println("仓库在备货");
+                                    orderService.updateOrderStatus(orderSn, 25, 10, "", "");
+                                }
+                                List<Map<String, Object>> products = (List<Map<String, Object>>) resMap.get("Products");//发货的数据
+                                for (Map<String, Object> product : products) {
+                                    String sku = (String) product.get("SKU");//发货商品规格编号
+                                    Integer quantity = Integer.valueOf(product.get("MaterialQuantity").toString());//发货商品数量
+                                    ShopGoodsSpec goodsSpec = shopGoodsSpecService.findByspecGoodsSerial(sku);//商品规格信息
+                                    Long goodsSpecId = goodsSpec.getId();//商品规格id
+                                    inventoryWarningService.updateInventoryByWareCodeAndSpecId("20192514", goodsSpecId, quantity);
+                                }
 
-                            List<ShopOrderGoods> shopOrderGoodsList = new ArrayList<>();
-                            List<ShopOrderGoods> orderGoodsList = (List<ShopOrderGoods>)resMap.get("orderGoods");
-                            List<ShopOrderGoods> shopOrderGoods = updateOrderGoods(shopOrderGoodsList, orderGoodsList, trackingNo);//需要修改订单商品信息
-                            shopOrderGoodsService.updateBatchForShipmentNum(shopOrderGoods);//修改订单商品信息
+                                List<ShopOrderGoods> shopOrderGoodsList = new ArrayList<>();
+                                List<ShopOrderGoods> orderGoodsList = (List<ShopOrderGoods>) resMap.get("orderGoods");
+                                List<ShopOrderGoods> shopOrderGoods = updateOrderGoods(shopOrderGoodsList, orderGoodsList, trackingNo);//需要修改订单商品信息
+                                shopOrderGoodsService.updateBatchForShipmentNum(shopOrderGoods);//修改订单商品信息
+                            }
                         }
-                    }
-                    if (success.equals("failure")) {//发货失败   提交状态：提交失败 失败原因：failInfo
-                        String failInfo = (String)maps.get("Info");//失败信息
-                        System.out.println("failInfo");
-                        orderService.updateOrderStatus(orderSn,20,20,failInfo,"");
+                        if (success.equals("failure")) {//发货失败   提交状态：提交失败 失败原因：failInfo
+                            String failInfo = (String) maps.get("Info");//失败信息
+                            System.out.println("failInfo");
+                            orderService.updateOrderStatus(orderSn, 20, 20, failInfo, "");
+                        }
                     }
                 }
             }
