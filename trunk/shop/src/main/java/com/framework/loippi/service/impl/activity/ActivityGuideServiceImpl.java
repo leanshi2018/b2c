2c7c1e745b280d3b43524112d05519b6f5471ccd
leanshi2018 <szleanshi@163.com>
2019-10-29 09:16:49 +0800

    20191029001

--- trunk/shop/src/main/java/com/framework/loippi/service/impl/activity/ActivityGuideServiceImpl.java
+++ trunk/shop/src/main/java/com/framework/loippi/service/impl/activity/ActivityGuideServiceImpl.java
@@ -3,6 +3,10 @@ package com.framework.loippi.service.impl.activity;
 import com.framework.loippi.entity.activity.ActivityGuide;
 import com.framework.loippi.service.activity.ActivityGuideService;
 import com.framework.loippi.service.impl.GenericServiceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class ActivityGuideServiceImpl extends GenericServiceImpl<ActivityGuide, Long> implements ActivityGuideService {
 }
