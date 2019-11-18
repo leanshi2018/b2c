03ddd5b4e59abae33bc33dc63b01cb019fdec537
kainoa <lkainoa1121@foxmail.com>
2019-11-18 11:09:13 +0800

    aa

--- trunk/admin/src/main/java/com/framework/loippi/controller/coupon/ShopCouponController.java
+++ trunk/admin/src/main/java/com/framework/loippi/controller/coupon/ShopCouponController.java
@@ -231,7 +231,7 @@ public class ShopCouponController extends GenericController {
      * @param
      * @return
      */
-    @RequestMapping(value = "/Coupon/findCouponPayDetailList",method = RequestMethod.POST)
+    @RequestMapping(value = "/Coupon/findCouponPayDetailList")
     public String findCouponPayDetailList(HttpServletRequest request,Pageable pageable,ModelMap model,@ModelAttribute CouponPayDetail param) {
         pageable.setParameter(param);
         pageable.setOrderProperty("create_time");
@@ -248,7 +248,7 @@ public class ShopCouponController extends GenericController {
      * @param param
      * @return
      */
-    @RequestMapping(value = "/Coupon/findCouponUserLogList",method = RequestMethod.POST)
+    @RequestMapping(value = "/Coupon/findCouponUserLogList")
     public String findCouponUseLogList(HttpServletRequest request,Pageable pageable,ModelMap model,@ModelAttribute CouponUserLogResult param) {
         pageable.setParameter(param);
         pageable.setOrderProperty("receive_time");
