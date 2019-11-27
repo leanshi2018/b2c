c9288029df78f3c8b748c79d6fa81950bf972adc
leanshi2018 <szleanshi@163.com>
2019-11-27 14:34:08 +0800

    修改优惠券创建时间

--- trunk/admin/src/main/java/com/framework/loippi/controller/coupon/ShopCouponController.java
+++ trunk/admin/src/main/java/com/framework/loippi/controller/coupon/ShopCouponController.java
@@ -2,6 +2,7 @@ package com.framework.loippi.controller.coupon;
 
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -150,16 +151,19 @@ public class ShopCouponController extends GenericController {
             model.addAttribute("msg", "优惠券结束使用时间为空");
             return Constants.MSG_URL;
         }
-        SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd 00:00:00");
-        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd 23:59:59");
-        String sendStartTime = format1.format(coupon.getSendStartTime());
-        String useStartTime = format1.format(coupon.getUseStartTime());
-        String sendEndTime = format2.format(coupon.getSendEndTime());
-        String useEndTime = format2.format(coupon.getUseEndTime());
-        coupon.setSendStartTime(new Date(sendStartTime));
-        coupon.setUseStartTime(new Date(useStartTime));
-        coupon.setSendEndTime(new Date(sendEndTime));
-        coupon.setUseEndTime(new Date(useEndTime));
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(coupon.getSendEndTime());
+        calendar.set(Calendar.HOUR_OF_DAY,23);
+        calendar.set(Calendar.MINUTE,59);
+        calendar.set(Calendar.SECOND,59);
+        calendar.set(Calendar.MILLISECOND,0);
+        coupon.setSendEndTime(calendar.getTime());
+        calendar.setTime(coupon.getUseEndTime());
+        calendar.set(Calendar.HOUR_OF_DAY,23);
+        calendar.set(Calendar.MINUTE,59);
+        calendar.set(Calendar.SECOND,59);
+        calendar.set(Calendar.MILLISECOND,0);
+        coupon.setUseEndTime(calendar.getTime());
         System.out.println(coupon);
         coupon.setId(couponId);
         Subject subject = SecurityUtils.getSubject();
