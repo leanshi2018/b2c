d96d8bc0124f031e6864c6124524aacac7d26061
kainoa <lkainoa1121@foxmail.com>
2020-01-08 16:22:50 +0800

    aa

--- trunk/admin/src/main/java/com/framework/loippi/controller/activity/ActivityCommonController.java
+++ trunk/admin/src/main/java/com/framework/loippi/controller/activity/ActivityCommonController.java
@@ -234,7 +234,7 @@ public class ActivityCommonController extends GenericController {
                 List<ShopHomePicture> pictureList = shopHomePictureService.findByTypeAndSort(shopHomePicture.getPictureType(),shopHomePicture.getPSort());
                 if (pictureList.size()!=0){
                     for (ShopHomePicture homePicture : pictureList) {
-                        if (homePicture.getId()!=shopHomePicture.getId()){
+                        if (homePicture.getId().longValue() != shopHomePicture.getId().longValue()){
                             if (homePicture.getPSort()==shopHomePicture.getPSort()){
                                 model.addAttribute("msg", "轮播图排序"+shopHomePicture.getPSort()+"号已存在");
                                 return Constants.MSG_URL;
@@ -252,7 +252,7 @@ public class ActivityCommonController extends GenericController {
                 List<ShopHomePicture> pictureList = shopHomePictureService.findByTypeAndSort(shopHomePicture.getPictureType(),shopHomePicture.getPSort());
                 if (pictureList.size()!=0){
                     for (ShopHomePicture homePicture : pictureList) {
-                        if (homePicture.getId()!=shopHomePicture.getId()){
+                        if (homePicture.getId().longValue() != shopHomePicture.getId().longValue()){
                             if (homePicture.getPSort()==shopHomePicture.getPSort()){
                                 model.addAttribute("msg", "广告位图排序"+shopHomePicture.getPSort()+"号已存在");
                                 return Constants.MSG_URL;
