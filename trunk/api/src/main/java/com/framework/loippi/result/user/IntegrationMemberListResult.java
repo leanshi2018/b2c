6e03c04a41cf664adbc56482a60227454ed599c5
leanshi2018 <szleanshi@163.com>
2020-04-15 16:11:44 +0800

    aa

--- trunk/api/src/main/java/com/framework/loippi/result/user/IntegrationMemberListResult.java
+++ trunk/api/src/main/java/com/framework/loippi/result/user/IntegrationMemberListResult.java
@@ -520,6 +520,14 @@ public class IntegrationMemberListResult {
 
                     }
                 }
+                //全都没找到的话
+                if (memberInfo.getMemberAvatarColour()==null){
+                    memberInfo.setGradeId(0);
+                    memberInfo.setGradeName(map.get(0));
+                    memberInfo.setMemberAvatarColour(3);
+                    memberInfo.setPpv(new BigDecimal("0.00"));
+                    memberInfo.setTotalPv(new BigDecimal("0.00"));
+                }
             }else {
                 memberInfo.setGradeId(0);
                 memberInfo.setGradeName(map.get(0));
