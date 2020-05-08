15c126c8542ca02d0c3baac3e1eee2f334c01715
leanshi2018 <szleanshi@163.com>
2020-05-08 16:38:36 +0800

    636363

--- trunk/api/src/test/java/Mytest.java
+++ trunk/api/src/test/java/Mytest.java
@@ -13,18 +13,18 @@ import com.framework.loippi.consts.AllInPayConstant;
 
 public class Mytest {
     public static void main(String[] args) {
-        //final String serverUrl = "https://fintech.allinpay.com/service/soa"; //测试环境请咨询对接人员
-        final String serverUrl = "http://116.228.64.55:6900/service/soa"; //测试环境请咨询对接人员(通商云门户测试环境地址)
-        final String sysId = "1902271423530473681";//商户私钥证书
-        final String pwd = "123456";//商户私钥密码
+        final String serverUrl = "https://fintech.allinpay.com/service/soa"; //测试环境请咨询对接人员
+        //final String serverUrl = "http://116.228.64.55:6900/service/soa"; //测试环境请咨询对接人员(通商云门户测试环境地址)
+        final String sysId = "2004261243386008175";//商户私钥证书
+        final String pwd = "leanshi20180501";//商户私钥密码
         //final String alias = "100000000002";
-        final String alias = "1902271423530473681";//商户公钥证书
+        final String alias = "2004261243386008175";//商户公钥证书
         //final String path = "D:\\tonglian-yun\\branches\\yuntestjava\\100000000002.pfx";
         //final String path = "C:\\Users\\zhangyu\\sign\\1902271423530473681.pfx";
-        final String path = "C:\\Users\\LDQ\\Desktop\\tongliansign\\1902271423530473681.pfx";
+        final String path = "D:\\formalSign\\2004261243386008175.pfx";
         //final String tlCertPath = "D:\\tonglian-yun\\branches\\yuntestjava\\TLCert-test.cer"; //生产环境请使用生产证书
         //final String tlCertPath = "C:\\Users\\zhangyu\\sign\\TLCert-test.cer"; //生产环境请使用生产证书
-        final String tlCertPath = "C:\\Users\\LDQ\\Desktop\\tongliansign\\TLCert-test.cer"; //生产环境请使用生产证书
+        final String tlCertPath = "D:\\formalSign\\TLCert(prod).cer"; //生产环境请使用生产证书
         final String version = "2.0";
         final YunConfig config = new YunConfig(serverUrl, sysId, pwd, alias, version, path,tlCertPath);
         YunClient.configure(config);
