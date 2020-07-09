2e9b60c62c430c0710ed05f9366badc443d56239
leanshi2018 <szleanshi@163.com>
2020-07-09 11:15:01 +0800

    验证短信

--- trunk/api/src/test/java/Mytest.java
+++ trunk/api/src/test/java/Mytest.java
@@ -37,7 +37,7 @@ public class Mytest {
         request.put("bizOrderNo", "123456");
         request.put("amount", 100L);
         request.put("fee", 0L);
-        request.put("validateType", 0L);
+        request.put("validateType", 1L);
         request.put("backUrl", AllInPayConstant.TRANSFER_BACKURL);
         request.put("industryCode", "2422");
         request.put("industryName", "其他");
@@ -46,7 +46,9 @@ public class Mytest {
         object1.put("amount",100L);
         object1.put("accountSetNo", AllInPayBillCutConstant.ACCOUNT_SET_NO);
         Map<String, Object> payMethods = new LinkedHashMap<>();
-        payMethods.put("BALANCE",object1);
+        ArrayList<Object> objects = new ArrayList<>();
+        objects.add(object1);
+        payMethods.put("BALANCE",objects);
         request.put("payMethod", payMethods);
         try {
             String s = YunClient.request(request);
@@ -64,10 +66,28 @@ public class Mytest {
      */
     @Test
     public void testCut() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY) - 1);
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String str = format.format(calendar.getTime());
-        System.out.println(str);
+        final String serverUrl = "https://fintech.allinpay.com/service/soa"; //测试环境请咨询对接人员
+        final String sysId = "2004261243386008175";//商户私钥证书
+        final String pwd = "leanshi20180501";//商户私钥密码
+        final String alias = "2004261243386008175";//商户公钥证书
+        final String path = "D:\\formalSign\\2004261243386008175.pfx";
+        final String tlCertPath = "D:\\formalSign\\TLCert(prod).cer"; //生产环境请使用生产证书
+        final String version = "2.0";
+        final YunConfig config = new YunConfig(serverUrl, sysId, pwd, alias, version, path,tlCertPath);
+        YunClient.configure(config);
+        final YunRequest request = new YunRequest("OrderService", "pay");
+        request.put("bizUserId", "900020102");
+        request.put("bizOrderNo", "123456");
+        request.put("verificationCode","");
+        request.put("consumerIp","192.168.1.88");
+        try {
+            String s = YunClient.request(request);
+            Map maps = (Map)JSON.parse(s);
+            String status = maps.get("status").toString();
+            System.out.println("status="+status);
+            System.out.println(maps);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
