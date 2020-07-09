63e05b20061ead76d9427f030b8690cb0ad1b42f
leanshi2018 <szleanshi@163.com>
2020-07-09 10:56:27 +0800

    222

--- trunk/base/src/main/java/com/framework/loippi/consts/AllInPayConstant.java
+++ trunk/base/src/main/java/com/framework/loippi/consts/AllInPayConstant.java
@@ -4,8 +4,8 @@ public interface AllInPayConstant {
     /**
      * 分账回调地址
      */
-    public static final String CUT_BILL_BACKURL="http://qa7kbb.natappfree.cc/admin_war_exploded/admin/allinpayContract/cutBack.jhtml";
-    //public static final String CUT_BILL_BACKURL="http://glht.rdnmall.cn/admin/admin/allinpayContract/cutBack.jhtml";
+    //public static final String CUT_BILL_BACKURL="http://qa7kbb.natappfree.cc/admin_war_exploded/admin/allinpayContract/cutBack.jhtml";
+    public static final String CUT_BILL_BACKURL="http://glht.rdnmall.cn/admin/admin/allinpayContract/cutBack.jhtml";
     /**
      * 通联网络签约地址 正式测试区分
      */
@@ -15,8 +15,8 @@ public interface AllInPayConstant {
     /**
      * 通联网络签约后台回调地址
      */
-    public static final String SIGN_BACK_URL="http://qa7kbb.natappfree.cc/admin_war_exploded/admin/allinpayContract/signBack.jhtml";
-    //public static final String SIGN_BACK_URL="http://glht.rdnmall.cn/admin/admin/allinpayContract/signBack.jhtml";
+    //public static final String SIGN_BACK_URL="http://qa7kbb.natappfree.cc/admin_war_exploded/admin/allinpayContract/signBack.jhtml";
+    public static final String SIGN_BACK_URL="http://glht.rdnmall.cn/admin/admin/allinpayContract/signBack.jhtml";
 
     /**
      * 通联网络签约app回调地址
@@ -25,6 +25,6 @@ public interface AllInPayConstant {
     /**
      * 通联用户转账回调地址
      */
-    //public static final String CUT_BILL_BACKURL="http://cnczhg.natappfree.cc/admin_war_exploded/admin/allinpayContract/transferBack.jhtml";
+    //public static final String TRANSFER_BACKURL="http://qa7kbb.natappfree.cc/admin_war_exploded/admin/allinpayContract/transferBack.jhtml";
     public static final String TRANSFER_BACKURL="http://glht.rdnmall.cn/admin/admin/allinpayContract/transferBack.jhtml";
 }
