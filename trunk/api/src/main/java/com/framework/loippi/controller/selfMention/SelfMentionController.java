53078eb777e80c22ff0816d67ac7f4ca680a1def
kainoa <lkainoa1121@foxmail.com>
2020-07-29 15:55:17 +0800

    aa

--- trunk/api/src/main/java/com/framework/loippi/controller/selfMention/SelfMentionController.java
+++ trunk/api/src/main/java/com/framework/loippi/controller/selfMention/SelfMentionController.java
@@ -340,6 +340,11 @@ public class SelfMentionController extends BaseController {
         int size = productResults.size();
         if (size==0){
             MentionProductVo resultPage = new MentionProductVo();
+            resultPage.setPageNum(0);
+            resultPage.setPageSize(10);
+            resultPage.setPages(1);
+            resultPage.setTotal(size);
+            resultPage.setProductResults(productResultList);
             return ApiUtils.success(resultPage);
         }else if (size<=10){
             currentPage = 1;
