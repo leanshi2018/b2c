abe5651f0783db6b5af34b8b191812114e943965
leanshi2018 <szleanshi@163.com>
2020-10-27 11:23:50 +0800

    10-27-002

--- trunk/shop/src/main/java/com/framework/loippi/result/app/cart/BaseGoodsResult.java
+++ trunk/shop/src/main/java/com/framework/loippi/result/app/cart/BaseGoodsResult.java
@@ -138,5 +138,60 @@ public class BaseGoodsResult {
         return results;
     }
 
+    public static <T extends BaseGoodsResult> List<T> buildList1(List<ShopOrderGoods> shopOrderGoods,Integer orderType, ResultFunction<T, ShopOrderGoods> fun)
+            throws Exception {
+        if (CollectionUtils.isEmpty(shopOrderGoods)) {
+            return Lists.newArrayList();
+        }
 
+        List<T> results = Lists.newArrayList();
+        for (ShopOrderGoods shopOrderGood : shopOrderGoods) {
+            if(shopOrderGood.getIsPresentation()==null||shopOrderGood.getIsPresentation()==0){
+                Optional<ShopOrderGoods> optOrderGoods = Optional.ofNullable(shopOrderGood);
+                T baseGoodsResult = fun.callback(shopOrderGood);
+                baseGoodsResult.setGoodsId(optOrderGoods.map(ShopOrderGoods::getGoodsId).orElse(-1L));
+                baseGoodsResult.setGoodsName(optOrderGoods.map(ShopOrderGoods::getGoodsName).orElse(""));
+//            baseGoodsResult.setGoodsStorePrice(NumberUtils.format(shopOrderGood.getGoodsPayPrice()));
+                // todo 订单实际支付价格
+                baseGoodsResult.setGoodsMarketPrice(NumberUtils.format(shopOrderGood.getGoodsPayPrice()));
+                baseGoodsResult.setDefaultImage(optOrderGoods.map(ShopOrderGoods::getGoodsImage).orElse(""));
+                baseGoodsResult.setQuantity(optOrderGoods.map(ShopOrderGoods::getGoodsNum).orElse(0));
+                baseGoodsResult.setSpecInfo(optOrderGoods.map(ShopOrderGoods::getSpecInfo).orElse(""));
+                baseGoodsResult.setSpecId(optOrderGoods.map(ShopOrderGoods::getSpecId).orElse(-1L));
+                if(orderType==3||orderType==8){
+                    baseGoodsResult.setPpv(optOrderGoods.map(ShopOrderGoods::getBigPpv).orElse(BigDecimal.ZERO));
+                }else {
+                    baseGoodsResult.setPpv(optOrderGoods.map(ShopOrderGoods::getPpv).orElse(BigDecimal.ZERO));
+                }
+                baseGoodsResult.setVipPrice(optOrderGoods.map(ShopOrderGoods::getVipPrice).orElse(BigDecimal.ZERO));
+                baseGoodsResult.setBigPpvPrice(optOrderGoods.map(ShopOrderGoods::getGoodsPrice).orElse(BigDecimal.ZERO));
+//            if (shopOrderGood.getShippingExpressId()!=null && shopOrderGood.getShippingCode()!=null && !"".equals(shopOrderGood.getShippingCode())){
+//                baseGoodsResult.setIsShipment(1);
+//            }else{
+//                baseGoodsResult.setIsShipment(0);
+//            }
+                results.add(baseGoodsResult);
+            }
+/*            Optional<ShopOrderGoods> optOrderGoods = Optional.ofNullable(shopOrderGood);
+            T baseGoodsResult = fun.callback(shopOrderGood);
+            baseGoodsResult.setGoodsId(optOrderGoods.map(ShopOrderGoods::getGoodsId).orElse(-1L));
+            baseGoodsResult.setGoodsName(optOrderGoods.map(ShopOrderGoods::getGoodsName).orElse(""));
+//            baseGoodsResult.setGoodsStorePrice(NumberUtils.format(shopOrderGood.getGoodsPayPrice()));
+            // todo 订单实际支付价格
+            baseGoodsResult.setGoodsMarketPrice(NumberUtils.format(shopOrderGood.getGoodsPayPrice()));
+            baseGoodsResult.setDefaultImage(optOrderGoods.map(ShopOrderGoods::getGoodsImage).orElse(""));
+            baseGoodsResult.setQuantity(optOrderGoods.map(ShopOrderGoods::getGoodsNum).orElse(0));
+            baseGoodsResult.setSpecInfo(optOrderGoods.map(ShopOrderGoods::getSpecInfo).orElse(""));
+            baseGoodsResult.setSpecId(optOrderGoods.map(ShopOrderGoods::getSpecId).orElse(-1L));
+            baseGoodsResult.setPpv(optOrderGoods.map(ShopOrderGoods::getPpv).orElse(BigDecimal.ZERO));
+//            if (shopOrderGood.getShippingExpressId()!=null && shopOrderGood.getShippingCode()!=null && !"".equals(shopOrderGood.getShippingCode())){
+//                baseGoodsResult.setIsShipment(1);
+//            }else{
+//                baseGoodsResult.setIsShipment(0);
+//            }
+            results.add(baseGoodsResult);*/
+        }
+
+        return results;
+    }
 }
