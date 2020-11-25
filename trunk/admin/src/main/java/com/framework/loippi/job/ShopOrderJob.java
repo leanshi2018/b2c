493f815185eb44246c0b35fa49296990da2cffc3
leanshi2018 <szleanshi@163.com>
2020-11-25 16:38:16 +0800

    11-25-003

--- trunk/admin/src/main/java/com/framework/loippi/job/ShopOrderJob.java
+++ trunk/admin/src/main/java/com/framework/loippi/job/ShopOrderJob.java
@@ -2246,8 +2246,8 @@ public class ShopOrderJob {
     /**
      * 执行定时发放plus会员积分奖励
      */
-    //@Scheduled(cron = "0 40 1 * * ? " )  //每隔一小时执行一次 每天1点40分执行定时任务
-    @Scheduled(cron = "0/45 * * * * ? " )  //每隔45秒执行一次
+    @Scheduled(cron = "0 40 1 * * ? " )  //每隔一小时执行一次 每天1点40分执行定时任务
+    //@Scheduled(cron = "0/45 * * * * ? " )  //每隔45秒执行一次
     public void grantPlusProfit(){
         System.out.println("plus奖励发放开始");
         plusProfitService.grantPlusProfit();
