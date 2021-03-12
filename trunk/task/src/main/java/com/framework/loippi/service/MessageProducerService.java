622a4a21a94186967290c0ff7c2d711770859904
leanshi2018 <szleanshi@163.com>
2021-03-12 14:49:56 +0800

    3-12-002

--- trunk/task/src/main/java/com/framework/loippi/service/MessageProducerService.java
+++ trunk/task/src/main/java/com/framework/loippi/service/MessageProducerService.java
@@ -1,12 +1,5 @@
 package com.framework.loippi.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-
 /**
  *
  *
